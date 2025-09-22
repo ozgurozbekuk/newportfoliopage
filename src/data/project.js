a0929d04389b11ba2d23498a0255abfd6711c558
ozgurozbekuk <ozgurozbek.uk@gmail.com>
2025-09-22 21:18:45 +0100

    twitter update

--- src/data/project.js
+++ src/data/project.js
@@ -5,19 +5,17 @@ const PROJECTS = [
     description:
       "A modern task management application featuring drag-and-drop task flow, an integrated Pomodoro timer, and a time tracker that records how much time you spend on each task. Easily organize your workflow, boost productivity, and stay on top of your progress—all in one place.",
     tech: ["React", "Context", "Tailwind", "Firebase"],
-    image:
-      "images/minutly.png",
+    image: "images/minutly.png",
     github: "https://github.com/ozgurozbekuk/time-manager",
     demo: "https://minutly.netlify.app/",
   },
   {
     title: "Twitter Clone",
-    categories: ["Fullstack", "React","AI"],
+    categories: ["Fullstack", "React", "AI"],
     description:
-      "A full-stack Twitter clone built with the MERN stack (MongoDB, Express, React, Node.js). It features JWT authentication, a modern React + Tailwind CSS frontend, and efficient data fetching with React Query. Users can register, log in, create posts, like, and comment in real time, providing a social media experience similar to Twitter.",
-    tech: ["React","Node.js","MongoDB","React Query", "Tailwind"],
-    image:
-      "images/twitter.png",
+      "A full-stack Twitter clone built with the MERN stack (MongoDB, Express, React, Node.js). It features JWT authentication, a modern React + Tailwind CSS frontend, and efficient data fetching with React Query. Users can register, log in, create posts, like, and comment in real time, providing a social media experience similar to Twitter.Note: Since the app is deployed on Render's free tier, it may take **30–60 seconds** to load on the first request.",
+    tech: ["React", "Node.js", "MongoDB", "React Query", "Tailwind"],
+    image: "images/twitter.png",
     github: "https://github.com/ozgurozbekuk/twitterX--clone",
     demo: "https://twitter-clone-k81w.onrender.com/",
   },
@@ -26,9 +24,8 @@ const PROJECTS = [
     categories: ["WordPress", "Real Life"],
     description:
       "Developed a responsive and SEO-friendly WordPress website in German for a moving and cleaning company in Austria, effectively showcasing their services.",
-    tech: ["WordPress","Divi", "ACF", "CSS", "SEO"],
-    image:
-      "images/entrumservice.png",
+    tech: ["WordPress", "Divi", "ACF", "CSS", "SEO"],
+    image: "images/entrumservice.png",
     github: "https://github.com/ozgurozbekuk/",
     demo: "https://entrumservice.at/",
   },
@@ -37,9 +34,8 @@ const PROJECTS = [
     categories: ["WordPress", "Real Life"],
     description:
       " A Car Rental site for a real client focusing on speed, SEO, and easy content management.",
-    tech: ["WordPress","Divi", "ACF", "CSS", "SEO"],
-    image:
-      "images/bosscarrental.png",
+    tech: ["WordPress", "Divi", "ACF", "CSS", "SEO"],
+    image: "images/bosscarrental.png",
     github: "https://github.com/ozgurozbekuk/",
     demo: "https://bosscarental.com",
   },
@@ -48,42 +44,37 @@ const PROJECTS = [
     categories: ["React"],
     description:
       " React portfolio website project with Framer Motion animations",
-    tech: ["React","Motion", "Three.js", "CSS"],
-    image:
-      "images/3Dportfolio.png",
+    tech: ["React", "Motion", "Three.js", "CSS"],
+    image: "images/3Dportfolio.png",
     github: "https://github.com/ozgurozbekuk/3Dportfolio",
     demo: "https://portfoliowebsite3d.netlify.app/",
   },
   {
     title: "Portfolio Web Site",
-    categories:[ "React"],
+    categories: ["React"],
     description:
       "Responsive, minimal and clear Portfolio Web Site. This Web Site",
-    tech: ["React", "Tailwind","react-lucide","Motion","Gemini Api"],
-    image:
-      "images/lastportfolio.png",
+    tech: ["React", "Tailwind", "react-lucide", "Motion", "Gemini Api"],
+    image: "images/lastportfolio.png",
     github: "https://github.com/ozgurozbekuk/newportfoliolast",
     demo: "https://ozgurpersonalpage.netlify.app/",
   },
   {
     title: "Personal Web Site using react",
-    categories:[ "React"],
-    description:
-      "Responsive, minimal and clear Personal Web Site.",
-    tech: ["React", "Tailwind","react-lucide"],
-    image:
-      "images/portfolioreact.png",
+    categories: ["React"],
+    description: "Responsive, minimal and clear Personal Web Site.",
+    tech: ["React", "Tailwind", "react-lucide"],
+    image: "images/portfolioreact.png",
     github: "https://github.com/ozgurozbekuk/personal-website",
     demo: "https://ozgurozbek.netlify.app/",
   },
   {
     title: "An AI-powered full-stack note-taking app",
-    categories: ["Fullstack", "React","AI"],
+    categories: ["Fullstack", "React", "AI"],
     description:
       "An AI-powered full-stack note-taking app built with React, Tailwind CSS, DaisyUI, Node.js, and MongoDB. It combines a clean UI with robust back-end architecture and intelligent features for smarter note management.",
-    tech: ["React","Nodejs", "MongoDB", "Tailwind", "Daisyui"],
-    image:
-      "images/thinkboard.png",
+    tech: ["React", "Nodejs", "MongoDB", "Tailwind", "Daisyui"],
+    image: "images/thinkboard.png",
     github: "https://github.com/ozgurozbekuk/mern-thinkboard",
     demo: "https://mern-thinkboard-vi0t.onrender.com/",
   },
