b3cc1a811bd540b21f100a2f0ef9e1711d7dd443
ozgurozbekuk <ozgurozbek.uk@gmail.com>
2026-02-17 22:31:28 +0000

    added asylum app

--- src/data/project.js
+++ src/data/project.js
@@ -1,20 +1,42 @@
 const PROJECTS = [
   {
-    title: "InvestorApp — A social platform where investors share insights, trends, and ideas in real time.",
-    categories: ["Next.js","Fullstack","AI"],
+    title:
+      " AI application built with a Retrieval-Augmented Generation (RAG) architecture to provide reliable, context-aware information about the UK asylum and immigration process.",
+    categories: ["Next.js", "Fullstack", "AI"],
+    description:
+      "Asylum Assistant is a production-ready AI application built with a Retrieval-Augmented Generation (RAG) architecture to provide reliable, context-aware information about the UK asylum and immigration process.The system retrieves relevant content from curated and trusted sources, then generates responses using a large language model grounded in that retrieved context. This approach improves factual accuracy and reduces hallucinations compared to standard prompt-only chat systems.The application is designed as an informational support tool and does not provide legal advice.",
+    tech: ["React", "Node.js", "", "MongoDB", "AI - RAG", "VPS deployed"],
+    image: "images/asyluminfo.png",
+    github: "https://github.com/ozgurozbekuk/asylumapp",
+    demo: "https://seekasylum.info/",
+  },
+  {
+    title:
+      "InvestorApp — A social platform where investors share insights, trends, and ideas in real time.",
+    categories: ["Next.js", "Fullstack", "AI"],
     description:
       "InvestorApp is the capstone project for the MIT Emerging Talent (ELO 2) program – It blends the familiar feed and interaction patterns of Twitter/Facebook with investor-centric features: market tickers, themed groups, and real-time reactions to price action. The goal is to demonstrate full-stack web skills and deliver a portfolio-ready, production-style product.",
-    tech: ["Next.js", "Tailwind","Typescript","PostgreSQL", "Prisma ORM"],
+    tech: ["Next.js", "Tailwind", "Typescript", "PostgreSQL", "Prisma ORM"],
     image: "images/invappfeedimage.png",
     github: "https://github.com/ozgurozbekuk/investorapp",
     demo: "https://investorapp.vercel.app/",
   },
   {
     title: "Minutly – Smart Task & Time Management App",
-    categories: ["React","Fullstack"],
+    categories: ["React", "Fullstack"],
     description:
       "Full-stack productivity suite that combines task planning, time tracking, and focus tools in one place. The project ships a React + Vite single-page application backed by an Express API with MongoDB persistence and JWT-secured authentication.",
-    tech: ["React","Node.js","MongoDB","Express.js", "Context", "Tailwind","Axios","JWT","CORS",],
+    tech: [
+      "React",
+      "Node.js",
+      "MongoDB",
+      "Express.js",
+      "Context",
+      "Tailwind",
+      "Axios",
+      "JWT",
+      "CORS",
+    ],
     image: "images/minutly.png",
     github: "https://github.com/ozgurozbekuk/time-manager",
     demo: "https://minutly.netlify.app/",
@@ -34,7 +56,7 @@ const PROJECTS = [
     categories: ["React", "AI"],
     description:
       "An interactive Interview Preparation App built with React, featuring over 600 questions and answers across various topics.The application is fully responsive, providing a smooth and accessible experience across desktop and mobile devices.Designed to support efficient study and practice with a clean and user-friendly interface.",
-    tech: ["CSS","React"],
+    tech: ["CSS", "React"],
     image: "images/interviewapp.png",
     github: "https://github.com/ozgurozbekuk/frontend-interview-question-app",
     demo: "https://interviewquestionapp.netlify.app/",
